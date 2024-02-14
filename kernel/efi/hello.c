fa616b1b272cb3a04100604234f7182283f5dcb9
Ryan Arora <arora.ryan@gmail.com>
2024-02-14 09:19:47 -0500

    print pci vendors and devices

--- kernel/efi/hello.c
+++ kernel/efi/hello.c
@@ -1,5 +1,114 @@
-#include <efi.h>
-#include <efilib.h>
+#include "efi.h"
+#include "efilib.h"
+
+static inline void outb(uint16_t port, uint8_t val) {
+	__asm__ volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+static inline void outw(uint16_t port, uint16_t val) {
+	__asm__ volatile("outw %w0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+static inline void outl(uint16_t port, uint32_t val) {
+	__asm__ volatile("outl %0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+static inline uint8_t inb(uint16_t port) {
+	uint8_t ret;
+	__asm__ volatile("inb %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
+	return ret;
+}
+
+static inline uint16_t inw(uint16_t port) {
+	uint16_t ret;
+	__asm__ volatile("inw %w1, %w0" : "=a"(ret) : "Nd"(port) : "memory");
+	return ret;
+}
+
+static inline uint32_t inl(uint16_t port) {
+	uint32_t ret;
+	__asm__ volatile("inl %w1, %0" : "=a"(ret) : "Nd"(port) : "memory");
+	return ret;
+}
+
+void kprint(WCHAR *str) {
+	ST->ConOut->OutputString(ST->ConOut, str);
+}
+
+void kprintuw(uint16_t num, uint8_t base) {
+	if (base != 10 && base != 16)
+		return;
+
+	WCHAR nums[12];
+	WCHAR nums_rev[13];
+	size_t len = 0;
+
+	if (num == 0) {
+		kprint(L"0\r\n");
+		return;
+	}
+
+	while (num > 0) {
+		WCHAR digit = num % base;
+		if (digit < 10)
+			nums[len] = '0' + digit;
+		else
+			nums[len] = 'A' + digit - 10;
+		num /= base;
+		++len;
+	}
+
+	for (size_t i = 0; i < len; ++i) {
+		nums_rev[i] = nums[len - 1 - i];
+	}
+
+	nums_rev[len] = L'\0';
+
+	kprint(nums_rev);
+	kprint(L"\r\n");
+}
+
+uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {
+	uint32_t address;
+	uint32_t lbus  = (uint32_t)bus;
+	uint32_t lslot = (uint32_t)slot;
+	uint32_t lfunc = (uint32_t)func;
+	uint16_t tmp   = 0;
+
+	// Create configuration address as per Figure 1
+	address = (uint32_t)((lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xFC) | ((uint32_t)0x80000000));
+
+	// Write out the address
+	outl(0xCF8, address);
+	// Read in the data
+	// (offset & 2) * 8) = 0 will choose the first word of the 32-bit register
+	tmp = (uint16_t)((inl(0xCFC) >> ((offset & 2) * 8)) & 0xFFFF);
+	return tmp;
+}
+
+uint16_t pci_get_vendor_id(uint8_t bus, uint8_t slot) {
+	return pci_config_read_word(bus, slot, 0, 0);
+}
+
+uint16_t pci_get_device_id(uint8_t bus, uint8_t slot) {
+	return pci_config_read_word(bus, slot, 0, 2);
+}
+
+void pci_print_devices() {
+	for (size_t bus = 0; bus < 256; ++bus) {
+		for (size_t slot = 0; slot < 32; ++slot) {
+			uint16_t vendor = pci_get_vendor_id(bus, slot);
+			if (vendor == 0xFFFF)
+				continue;
+			uint16_t device = pci_get_device_id(bus, slot);
+
+			kprint(L"vendor=");
+			kprintuw(vendor, 16);
+			kprint(L"device=");
+			kprintuw(device, 16);
+		}
+	}
+}
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	EFI_STATUS Status;
@@ -8,9 +117,8 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	/* Store the system table for future use in other functions */
 	ST = SystemTable;
 
-	/* Say hi */
-	Status = ST->ConOut->OutputString(ST->ConOut, L"Hello, world!\r\n"); // EFI Applications use Unicode and
-																		 // CRLF, a la Windows
+	pci_print_devices();
+
 	if (EFI_ERROR(Status))
 		return Status;
 

b8939885ce927598f0c1423fda916a815dc0cdb0
Ryan Arora <arora.ryan@gmail.com>
2024-02-19 12:29:34 -0500

    panic macro prints file and line number

--- kernel/include/kernel/lib/panic.h
+++ kernel/include/kernel/lib/panic.h
@@ -1,3 +1,11 @@
 #pragma once
 
 __declspec(noreturn) void panic(void);
+
+#define panic()                                                                                                                                                \
+	do {                                                                                                                                                       \
+		kprintf("FATAL: kernel panic at %s:%d\n", __FILE__, __LINE__);                                                                                         \
+		while (1)                                                                                                                                              \
+			__asm__("hlt");                                                                                                                                    \
+                                                                                                                                                               \
+	} while (0)

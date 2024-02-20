d9be08ab22a996a966f59079473240969db1a2c6
Ryan Arora <arora.ryan@gmail.com>
2024-02-20 01:44:22 -0500

    add memset string function

--- kernel/lib/string.c
+++ kernel/lib/string.c
@@ -1,10 +1,17 @@
 #include <kernel/lib/string.h>
 
 void *memcpy(void *dest, const void *src, size_t n) {
-	for (size_t i = 0; i < n; ++i) {
+	for (size_t i = 0; i < n; ++i)
 		((char *)dest)[i] = ((const char *)src)[i];
-	}
+
 	return dest;
+}
+
+void *memset(void *s, char c, size_t n) {
+	for (size_t i = 0; i < n; ++i)
+		((char *)(s))[i] = c;
+
+	return s;
 }
 
 size_t strlen(const char *s) {

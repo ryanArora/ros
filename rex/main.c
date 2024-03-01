97efa06a1725fcc5aedb9ea46d1c244b607a043d
Ryan Arora <arora.ryan@gmail.com>
2024-03-01 15:48:42 -0500

    rex initial commit, switch to C++ for LLVM

--- rex/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv) {
-	printf("rex compiler\n");
-	return EXIT_SUCCESS;
-}
---
+++ rex/main.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+void usage(const char* program_name);
+int main(int argc, char* argv[]);
+
+void run_usage(const char* program_name);
+int run_main(int argc, char* argv[]);

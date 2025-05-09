efbac768f47bdd6592438ab295afb45d4fcbcfee
Ryan Arora <arora.ryan@gmail.com>
2025-05-09 18:37:48 -0400

    for now, no open/close

--- kernel/fs/fs.c
+++ kernel/fs/fs.c
@@ -3,6 +3,8 @@
 #include "fat16.h"
 #include <stddef.h>
 
+#define FS_MAX_OPEN_FILES 512;
+
 const struct fs*
 fs_probe(size_t device_id)
 {
@@ -13,4 +15,20 @@ fs_probe(size_t device_id)
     } else {
         return NULL;
     }
+}
+
+void
+fs_read(const char* path, void* buf, size_t count)
+{
+    (void)path;
+    (void)buf;
+    (void)count;
+}
+
+void
+fs_write(const char* path, const void* buf, size_t count)
+{
+    (void)path;
+    (void)buf;
+    (void)count;
 }
--- kernel/fs/fs.h
+++ kernel/fs/fs.h
@@ -7,3 +7,6 @@ struct fs {
 };
 
 const struct fs* fs_probe(size_t device_id);
+
+void fs_read(const char* path, void* buf, size_t count);
+void fs_write(const char* path, const void* buf, size_t count);
